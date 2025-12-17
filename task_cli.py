2ffb2600b85334ce847bfb5379dc28d96125c9b7
Anurag Kumar <anuragkrs2195@gmail.com>
2025-12-17 12:34:25 +0530

    feat: add task add, update, delete, and status commands

--- task_cli.py
+++ task_cli.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 FILE_NAME="tasks.json"
 
-def load_task():
+def load_tasks():
     if not os.path.exists(FILE_NAME):
         with open(FILE_NAME,'w') as f:
             json.dump([],f)
@@ -21,3 +21,56 @@ def get_next_id(tasks):
         return 1
     return max(task["id"] for task in tasks) + 1
 
+def add_task(description):
+    tasks = load_tasks()
+    task = {
+        "id": get_next_id(tasks),
+        "description": description,
+        "status": "todo",
+        "createdAt": datetime.now().isoformat(),
+        "updatedAt": datetime.now().isoformat()
+    }
+    tasks.append(task)
+    save_tasks(tasks)
+    print(f"Task added successfully (ID: {task['id']})")
+
+def update_task(task_id, description):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["description"] = description
+            task["updatedAt"] = datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} updated successfully!")
+            return
+    print("Task not found!")
+
+def delete_task(task_id):
+    tasks = load_tasks()
+    new_tasks = [t for t in tasks if t["id"] != task_id]
+    if len(new_tasks) == len(tasks):
+        print("Task not found!")
+        return
+    save_tasks(new_tasks)
+    print(f"Task {task_id} deleted successfully!")
+
+def mark_status(task_id, status):
+    tasks = load_tasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = status
+            task["updatedAt"] = datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} marked as {status}.")
+            return
+    print("Task not found!")
+
+def list_tasks(filter_status=None):
+    tasks = load_tasks()
+    if filter_status:
+        tasks = [t for t in tasks if t["status"] == filter_status]
+    if not tasks:
+        print("No tasks found.")
+        return
+    for task in tasks:
+        print(f"{task['id']}. [{task['status']}] {task['description']}")
